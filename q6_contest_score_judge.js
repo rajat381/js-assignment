affa624aceec8ce66094f5420829f3f5cc8c3b2e
Rajat <rajatdsingh15@gmail.com>
2026-04-15 11:02:49 +0530

    Update q6_contest_score_judge.js

--- q6_contest_score_judge.js
+++ q6_contest_score_judge.js
@@ -3,13 +3,17 @@
 let a = parseInt(prompt("Enter correct answers (a):"));
 let b = parseInt(prompt("Enter partially correct answers (b):"));
 let c = parseInt(prompt("Enter wrong answers (c):"));
+
 let score = 3 * a + b - 2 * c;
-if (score < 0) {
-    score = 0;
-}
+
 if (a + b + c > 50) {
     score = score - 10;
 }
+
+
+    score = 0;
+}
+
 let status = (score >= 60) ? "PASS" : "FAIL";
 
 alert(score + ", " + status);
